d5b29d63cc4e84b879ecdaca95a8dd2d1e78a105
faisal-saddique <faisalsaddique7964@gmail.com>
2023-02-13 23:40:08 +0500

    added working flow

--- settings.js
+++ settings.js
@@ -121,14 +121,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    //adminAuth: {
-    //    type: "credentials",
-    //    users: [{
-    //        username: "admin",
-    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-    //        permissions: "*"
-    //    }]
-    //},
+    adminAuth: {
+       type: "credentials",
+       users: [{
+           username: "admin",
+           password: "$2b$08$cxGoIBVwciNVXFLsiPz.IuzWL5Z5heqRNRW4xMcv1S1zMjrdn.ah6",
+           permissions: "*"
+       }]
+    },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
@@ -214,10 +214,11 @@ module.exports = {
     //    functionGlobalContext: { os:require('os') }
     // can be accessed in a function block as:
     //    global.get("os")
+    functionExternalModules: true,
     functionGlobalContext: {
-        // os:require('os'),
-        // jfive:require("johnny-five"),
-        // j5board:require("johnny-five").Board({repl:false})
+        os:require('os'),
+        fs: require('fs'),
+        moment: require('moment')
     },
     // `global.keys()` returns a list of all properties set in global context.
     // This allows them to be displayed in the Context Sidebar within the editor.
@@ -268,9 +269,13 @@ module.exports = {
 
     // Customising the editor
     editorTheme: {
+        // zenburn, tokyo-night, solarized-light, solarized-dark, oled,
+        // oceanic-next, monokai, monoindustrial, midnight-red, espresso-libre
+        // dracula, dark, cobalt2, aurora
+        theme: "oceanic-next",
         projects: {
             // To enable the Projects feature, set this value to true
-            enabled: false
+            enabled: true
         }
     }
 };
